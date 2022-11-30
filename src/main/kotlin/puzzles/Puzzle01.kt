93a78eca637b356ce5de0cc5ecc6330c69fc5d23
Martin Weber <martin.weber@teamecho.at>
2022-11-30 07:50:36 +0100

    imporve base structure

---
+++ src/main/kotlin/puzzles/Puzzle01.kt
@@ -0,0 +1,25 @@
+package puzzles
+
+import Puzzle
+
+class Puzzle00P1 : Puzzle() {
+    override fun solve(lines : List<String>): String {
+        var counter = -1
+        var lastDepth = -1
+        for (line in lines) {
+            val currentDepth = line.toInt()
+            if (currentDepth > lastDepth) {
+                counter++
+            }
+            lastDepth = currentDepth
+        }
+        return counter.toString()
+    }
+}
+
+class Puzzle00P2 : Puzzle() {
+    override fun solve(lines: List<String>): String {
+        TODO("Part 2 is not yet implemented")
+    }
+
+}
