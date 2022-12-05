c09c7e6d1c5fde2d4e430117897cd2b917770e1b
Martin Weber <martin.weber@teamecho.at>
2022-12-05 06:52:13 +0100

    adapt day 00 template

--- src/main/kotlin/puzzles/Puzzle00.kt
+++ src/main/kotlin/puzzles/Puzzle00.kt
@@ -4,22 +4,13 @@ import Puzzle
 
 class Puzzle00P1 : Puzzle() {
     override fun solve(lines : List<String>): String {
-        var counter = -1
-        var lastDepth = -1
-        for (line in lines) {
-            val currentDepth = line.toInt()
-            if (currentDepth > lastDepth) {
-                counter++
-            }
-            lastDepth = currentDepth
-        }
-        return counter.toString()
+        TODO("$name is not yet implemented")
     }
 }
 
 class Puzzle00P2 : Puzzle() {
     override fun solve(lines: List<String>): String {
-        TODO("Puzzle 0 Part 2 is not yet implemented")
+        TODO("$name is not yet implemented")
     }
 
 }
