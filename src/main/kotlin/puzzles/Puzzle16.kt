1b44d3e4a9a5bdd5da61eb7cbf9e052491911307
Martin Weber <martin.weber@teamecho.at>
2022-12-18 16:44:00 +0100

    day 16 part 2 clean up

--- src/main/kotlin/puzzles/Puzzle16.kt
+++ src/main/kotlin/puzzles/Puzzle16.kt
@@ -62,57 +62,8 @@ class Puzzle16 {
             return bestFinalState!!.pressureReleased
         }
 
-//        private fun shortestPath1(
-//            states: PriorityQueue<State>,
-//            navigatableMap: Map<String, Valve>,
-//        ): Int {
-//            val visited = mutableSetOf<Int>()
-//            var bestFinalState: State? = null
-//
-//            while (states.isNotEmpty()) {
-//                val first = states.first()
-//                states.remove(first)
-//                if (first.minutesLeft == 0) {
-//                    if (bestFinalState == null || bestFinalState.pressureReleased < first.pressureReleased) {
-//                        bestFinalState = first
-//                    }
-//
-//                    continue;
-//                }
-//                if ((30 - first.minutesLeft) > (first.openedValved.size + 1) * navigatableMap.size) {
-//                    continue
-//                }
-//                if (first.minutesLeft >= 1) {
-//                    if (first.position.flowRate > 0 && !first.openedValved.contains(first.position)) {
-//                        val open = State(
-//                            first.position,
-//                            first.openedValved + first.position,
-//                            first.pressureReleased + first.position.flowRate * (first.minutesLeft - 1),
-//                            first.minutesLeft - 1
-//                        )
-//                        if (!visited.contains(open.hashCode())) {
-//                            states.add(open)
-//                            visited.add(open.hashCode())
-//                        }
-//                    }
-//                    for (c in navigatableMap.getValue(first.position.code).connections) {
-//                        val cV = navigatableMap.getValue(c)
-//                        val notOpen = State(cV, first.openedValved, first.pressureReleased, first.minutesLeft - 1)
-//                        if (!visited.contains(notOpen.hashCode())) {
-//                            states.add(notOpen)
-//                            visited.add(notOpen.hashCode())
-//                        }
-//                    }
-//                }
-//            }
-//            return bestFinalState!!.pressureReleased
-//        }
-
         data class State(
-            val position: Valve,
-            val openedValved: Set<Valve>,
-            val pressureReleased: Int,
-            val minutesLeft: Int
+            val position: Valve, val openedValved: Set<Valve>, val pressureReleased: Int, val minutesLeft: Int
         ) : Comparable<State> {
             override fun compareTo(other: State): Int {
                 val diff = (other.pressureReleased - pressureReleased)
@@ -129,13 +80,10 @@ class Puzzle16 {
     class Part2 : Puzzle() {
         override fun solve(lines: List<String>): String {
             val valveMap = parseValves(lines)
-//            valveMap
-//                .mapValues { (k,v) -> Valve(v.code, v.flowRate, v.connections.map{ valveMap.getValue(it) }) }
 
             val queue = PriorityQueue<State2>()
             val aa = valveMap.getValue("AA")
             queue.add(State2(setOf(aa), emptySet(), 0, 26))
-//            queue.add(State2(aa, aa, emptySet(), valveMap.values.toSet(), 0, 26))
             val s = shortestPath(queue, valveMap)
 
             return s.toString()
@@ -170,9 +118,6 @@ class Puzzle16 {
                 }
 
 
-//                if ((30 - first.minutesLeft) > (first.openedValved.size + 1) * navigatableMap.size) {
-//                    continue
-//                }
                 if (first.minutesLeft >= 1) {
                     val positionSteps = getSteps(first.position.first(), first, navigatableMap)
                     val elephantSteps = getSteps(first.position.last(), first, navigatableMap)
@@ -184,11 +129,9 @@ class Puzzle16 {
 
                             val n = State2(
                                 setOf(
-                                    p.newPosition,
-                                    e.newPosition
+                                    p.newPosition, e.newPosition
                                 ),
-                                        openedValves = first . openedValves +openedValves,
-//                                closedValved = first.closedValved- openedValves,
+                                openedValves = first.openedValves + openedValves,
                                 pressureReleased = first.pressureReleased + flow * (first.minutesLeft - 1),
                                 minutesLeft = first.minutesLeft - 1
                             )
@@ -205,9 +148,7 @@ class Puzzle16 {
         }
 
         private fun getSteps(
-            position: Valve,
-            first: State2,
-            navigatableMap: Map<String, Valve>
+            position: Valve, first: State2, navigatableMap: Map<String, Valve>
         ): List<Step> {
             val positionSteps = mutableListOf<Step>()
             if (position.flowRate > 0 && !first.openedValves.contains(position)) {
@@ -223,7 +164,6 @@ class Puzzle16 {
 
         data class BestOf(
             val position: Set<Valve>,
-//            val elephant: Valve,
             val openedValves: Set<Valve>,
         )
 
@@ -233,32 +173,17 @@ class Puzzle16 {
         )
 
         data class State2(
-            val position: Set<Valve>,
-            val openedValves: Set<Valve>,
-//            val closedValved : Set<Valve>,
-            val pressureReleased: Int,
-            val minutesLeft: Int
+            val position: Set<Valve>, val openedValves: Set<Valve>, val pressureReleased: Int, val minutesLeft: Int
         ) : Comparable<State2> {
 
             var atBest = -1
 
             init {
                 atBest = pressureReleased
-//                + closedValved.map { it.flowRate }.sortedDescending().take(minutesLeft)
-//                    .mapIndexed { index, i -> i * (minutesLeft - index) }.sum()
-//                if (position in closedValved)
-//                {
-//                    position.flowRate * (minutesLeft-1)
-//                }
-//                else
-//                {
-//                    0
-//                }
             }
 
             override fun compareTo(other: State2): Int {
                 val diff = (other.atBest - atBest)
-//                return diff
                 if (diff != 0) {
                     return diff
                 } else {
@@ -269,7 +194,6 @@ class Puzzle16 {
     }
 
     class Valve(val code: String, val flowRate: Int, val connections: List<String>)
-//    class Valve(val code: String, val flowRate: Int, val connections: List<Valve>)
 
 
     companion object {
